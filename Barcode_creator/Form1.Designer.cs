76cfe04c4fa258113253e9b03d3d3d982546171a
senia1 <oks314@gmail.com>
2022-05-10 10:47:51 +0300

    to button click

--- Barcode_creator/Form1.Designer.cs
+++ Barcode_creator/Form1.Designer.cs
@@ -28,13 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(34, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(34, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(297, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "CODEBAR",
+            "CODE_39",
+            "CODE_93",
+            "CODE_128",
+            "QR_CODE",
+            "MSI",
+            "DATA_MATRIX"});
+            this.comboBox1.Location = new System.Drawing.Point(210, 55);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "CODEBAR";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(210, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(210, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Decipher";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(210, 97);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Download";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(377, 247);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Barcode_creator/Form1.cs
+++ Barcode_creator/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZXing;
+using ZXing.Rendering;
+using ZXing.QrCode;
+using ZXing.Common;
 
 namespace Barcode_creator
 {
@@ -15,6 +19,95 @@ namespace Barcode_creator
         public Form1()
         {
             InitializeComponent();
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDown;
+        }
+
+        public static Image CreateCode(string text, int w, int h, BarcodeFormat format) 
+        {
+            try
+            {
+                BarcodeWriter writer = new BarcodeWriter
+                {
+                    Format = format,
+                    Options = new QrCodeEncodingOptions
+                    {
+                        Width = w,
+                        Height = h,
+                        CharacterSet = "UTF-8"
+                    },
+                    Renderer = new BitmapRenderer()
+                };
+                return writer.Write(text);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return null;
+
+        }
+        public static string[] CodeScan(Bitmap bmp)
+        {
+            try
+            {
+                BarcodeReader reader = new BarcodeReader
+                {
+                    AutoRotate = true,
+                    TryInverted = true,
+                    Options = new DecodingOptions
+                    {
+                        TryHarder = true
+                    }
+                };
+                Result[] results = reader.DecodeMultiple(bmp);
+                if (results != null)
+                    return results.Where(x => x != null && !string.IsNullOrEmpty(x.Text)).Select(x => x.Text).ToArray();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return null;
+        }
+        public static string DecodeImage(Image img)
+        {
+            string outString = "";
+            string[] results = CodeScan((Bitmap)img);
+            if (results != null)
+                outString = string.Join(Environment.NewLine + Environment.NewLine, results);
+            return outString;
+        }
+        private BarcodeFormat GetFormat() 
+        {
+            switch (comboBox1.Text)
+            {
+                case "CODEBAR": return BarcodeFormat.CODABAR;
+                case "CODE_39": return BarcodeFormat.CODE_39;
+                case "CODE_93": return BarcodeFormat.CODE_93;
+                case "CODE_128": return BarcodeFormat.CODE_128;
+                case "QR_CODE": return BarcodeFormat.QR_CODE;
+                case "MSI": return BarcodeFormat.MSI;
+                case "DATA_MATRIX": return BarcodeFormat.DATA_MATRIX;
+                default: return BarcodeFormat.CODABAR;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
